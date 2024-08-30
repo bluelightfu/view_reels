6bb5a15007adf7627b672b72d6bd58aa4d15b2a0
Shao Fu Chen <abcdmarcus@cht.com.tw>
2024-08-30 16:32:52 +0800

    包裝成class，並用thread平行處理，大幅加速!

--- view_ig_reels.py
+++ view_ig_reels.py
@@ -2,45 +2,56 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 import json
+import threading
 
-def view_ig_reels(num_windows:int, target_ig_reels_url:str, *args, **kwargs):
-    # Setup Chrome options
-    chrome_options = Options()
-    chrome_options.add_experimental_option("detach", True)
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--incognito")
-    # chrome_options.add_argument("--headless")  # Optional : run in the background
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("disable-popup-blocking")
-    browsers = []
-
-    for _ in range(num_windows):
-        # cookie免帳號登入
+class ViewIGReels(threading.Thread):
+    def __init__(self, url): 
+        threading.Thread.__init__(self) 
+        self.url = url 
+        self.browser = None
+        self.stop_flag = False
+  
+    def run(self): 
+        # Setup Chrome options
+        chrome_options = Options()
+        chrome_options.add_experimental_option("detach", True)
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--incognito")
+        # chrome_options.add_argument("--headless")  # Optional : run in the background
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("disable-popup-blocking")
         with open('cookies_ig.json') as f:
             cookies = json.load(f)
-        browser = webdriver.Chrome(options=chrome_options)
-        browser.get('https://www.instagram.com/')
+        self.browser = webdriver.Chrome(options=chrome_options)
+        self.browser.get('https://www.instagram.com/')
 
         for cookie in cookies:
-            browser.add_cookie(cookie)
-        browser.refresh()
-        browser.get(target_ig_reels_url)
-
-        browsers.append(browser)
-
-    return browsers
+            self.browser.add_cookie(cookie)
+        self.browser.refresh()
+        self.browser.get(target_ig_reels_url)
+        while not self.stop_flag:
+            time.sleep(90)
+            if self.stop_flag:
+                break
+            self.browser.refresh()
+        self.browser.quit()
+    def stop(self):
+        self.stop_flag = True
 
 if __name__ == "__main__":
-    number_of_windows = 5
-    target_ig_reels_url = "https://www.instagram.com/reel/C8YgP_wB9Wn/"
-    browsers = view_ig_reels(number_of_windows, target_ig_reels_url)
+    number_of_windows = 2
+    target_ig_reels_url = "https://www.instagram.com/reel/XXXX" # target url
 
-    try:
-        while True:
-            time.sleep(90)  # 每 90 秒重整
-            for browser in browsers:
-                browser.refresh()  # 重整
-    except KeyboardInterrupt:
-        for browser in browsers:
-            browser.quit()  #完整關閉需要時間，請耐心等候
+    threads = []
+    urls = [target_ig_reels_url for _ in range(number_of_windows)]
+    for url in urls: 
+        t = ViewIGReels(url) 
+        t.start()
+        threads.append(t)
+        
+    input("press any key to quit")
+    for t in threads:
+        t.stop()
+
+        
