517b14e87ea74b76171c571e56334c94bf18bd4d
Shao Fu Chen <abcdmarcus@cht.com.tw>
2024-08-30 17:05:53 +0800

    調整提示語句

--- view_ig_reels.py
+++ view_ig_reels.py
@@ -5,11 +5,12 @@ import json
 import threading
 
 class ViewIGReels(threading.Thread):
-    def __init__(self, url): 
+    def __init__(self, url, time_period=60): 
         threading.Thread.__init__(self) 
         self.url = url 
         self.browser = None
         self.stop_flag = False
+        self.time_period = time_period
   
     def run(self): 
         # Setup Chrome options
@@ -21,6 +22,7 @@ class ViewIGReels(threading.Thread):
         # chrome_options.add_argument("--headless")  # Optional : run in the background
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("disable-popup-blocking")
+
         with open('cookies_ig.json') as f:
             cookies = json.load(f)
         self.browser = webdriver.Chrome(options=chrome_options)
@@ -28,31 +30,37 @@ class ViewIGReels(threading.Thread):
 
         for cookie in cookies:
             self.browser.add_cookie(cookie)
+
         self.browser.refresh()
         self.browser.get(target_ig_reels_url)
+
         while not self.stop_flag:
-            time.sleep(90)
+            time.sleep(self.time_period)
             if self.stop_flag:
                 print("[INFO] 收到關閉指令!")
                 break
             self.browser.refresh()
+
         self.browser.quit()
+
     def stop(self):
         self.stop_flag = True
 
 if __name__ == "__main__":
-    number_of_windows = 2
-    target_ig_reels_url = "https://www.instagram.com/reel/XXXX" # target url
+    number_of_windows = 4 # windows count 視窗數
+    target_ig_reels_url = "https://www.instagram.com/reel/XXX/" # target url
+    refresh_period_time = 60 # refresh period time 頁面重整週期(sec)
 
     threads = []
     urls = [target_ig_reels_url for _ in range(number_of_windows)]
+
     for url in urls: 
-        t = ViewIGReels(url) 
-        t.start()
+        t = ViewIGReels(url, time_period=refresh_period_time)
+        t.start() # run()
         threads.append(t)
         
     input("[INFO] 輸入按任何鍵結束工作")
-    print("[INFO] 請耐心等候，視窗將會關閉")
+    print("[INFO] 請耐心等候，當視窗跑完該時間週期，視窗將會自動關閉!")
     for t in threads:
         t.stop()
 
